f38ac7523930a88c859e2c517d46b65c2db14ec7
svoshall <svoshall@vivsoft.io>
2024-01-24 12:44:32 -0600

    init lab 3

---
+++ kode-cloud-labs/multiple-providers/terraform-projects/christmas-wishlist/cyberpunk.tf
@@ -0,0 +1,4 @@
+resource "local_file" "cyberpunk" {
+  filename     = "/root/cyberpunk2077.txt"
+  content  = "All I need for Christmas is Cyberpunk 2077!"
+}
---
+++ kode-cloud-labs/multiple-providers/terraform-projects/christmas-wishlist/ps5.tf
@@ -0,0 +1,4 @@
+resource "local_file" "ps5" {
+  filename     = "/root/ps5.txt"
+  content  = "And a PS5!"
+}
