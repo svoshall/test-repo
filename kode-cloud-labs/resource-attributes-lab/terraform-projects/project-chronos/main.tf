840dfbaeb7d7bebf5af5e25c3adaf5dc6b99395a
svoshall <svoshall@vivsoft.io>
2024-01-29 15:07:09 -0600

    resource-atts-lab: added att id

--- kode-cloud-labs/resource-attributes-lab/terraform-projects/project-chronos/main.tf
+++ kode-cloud-labs/resource-attributes-lab/terraform-projects/project-chronos/main.tf
@@ -3,5 +3,5 @@ resource "time_static" "time_update" {
 
 resource "local_file" "time" {
     filename = "/root/time.txt"
-    content = "Time stamp of this file is <id from time_update resource>"
+    content = "Time stamp of this file is ${time_static.time_update.id}"
 }
