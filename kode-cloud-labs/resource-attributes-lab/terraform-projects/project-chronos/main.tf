d9b369f81012d785c924f57f594c72238d24074e
svoshall <svoshall@vivsoft.io>
2024-01-29 13:25:35 -0600

    resource-atts-lab: add resource time of local_file

--- kode-cloud-labs/resource-attributes-lab/terraform-projects/project-chronos/main.tf
+++ kode-cloud-labs/resource-attributes-lab/terraform-projects/project-chronos/main.tf
@@ -1,2 +1,7 @@
- resource "time_static" "time_update" {
+resource "time_static" "time_update" {
+}
+
+resource "local_file" "time" {
+    filename = "/root/time.txt"
+    content = "Time stamp of this file is <id from time_update resource>"
 }
