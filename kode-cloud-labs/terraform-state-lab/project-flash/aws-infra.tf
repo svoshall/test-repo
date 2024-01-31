96efaef8e6e24cae078950bd4212b0fcc70a5f29
svoshall <svoshall@vivsoft.io>
2024-01-31 16:34:18 -0600

    terraform-state-lab: fixed typo

--- kode-cloud-labs/terraform-state-lab/project-flash/aws-infra.tf
+++ kode-cloud-labs/terraform-state-lab/project-flash/aws-infra.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "dev-server" {
     instance_type = "t2.micro"
     ami         = "ami-02cff456777cd"
 }
-resource "aws_s3_bucket" "falshpoint"  {
+resource "aws_s3_bucket" "flashpoint"  {
     bucket = "project-flashpoint-paradox"
 }
