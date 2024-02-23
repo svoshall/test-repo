0044b830550ed6d6133b2fb691fb98799448a9a6
svoshall <svoshall@vivsoft.io>
2024-02-23 17:48:27 -0600

    lifecycle-rules-lab: new config

--- kode-cloud-labs/lifecycle-rules-lab/terraform-projects/project-mysterio/main.tf
+++ kode-cloud-labs/lifecycle-rules-lab/terraform-projects/project-mysterio/main.tf
@@ -1,19 +1,5 @@
-resource "local_file" "file" {
-    filename = var.filename
-    file_permission =  var.permission
-    content = random_string.string.id
-    lifecycle {
-        create_before_destroy =  false # not always the best rule to use - 2 files cannot be named the same
-    }
-}
-
-resource "random_string" "string" {
+resource "random_pet" "super_pet" {
     length = var.length
-    keepers = {
-        length = var.length
-    }  
+    prefix = var.prefix
     
-    lifecycle {
-        create_before_destroy = true
-    }
 }
--- kode-cloud-labs/lifecycle-rules-lab/terraform-projects/project-mysterio/variables.tf
+++ kode-cloud-labs/lifecycle-rules-lab/terraform-projects/project-mysterio/variables.tf
@@ -1,13 +1,7 @@
 variable "length" {
-    default = 12
-  
+    default = 10
+
 }
-variable "filename" {
-    default = "/root/random_text"
-}
-variable "content" {
-    default = "This file contains a single line of data"
-}
-variable "permission" {
-    default = 0700
+variable "prefix" {
+    default = "Mr"
 }
