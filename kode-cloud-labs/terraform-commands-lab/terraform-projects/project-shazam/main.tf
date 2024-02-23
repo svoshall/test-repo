afb0ed23c58cb0e22fafeac5f36c2fe9d674386f
svoshall <svoshall@vivsoft.io>
2024-02-23 17:05:16 -0600

    fmt - canonical formatting

--- kode-cloud-labs/terraform-commands-lab/terraform-projects/project-shazam/main.tf
+++ kode-cloud-labs/terraform-commands-lab/terraform-projects/project-shazam/main.tf
@@ -1,16 +1,16 @@
 resource "local_file" "key_data" {
-        filename       = "/tmp/.pki/private_key.pem"
-        content = tls_private_key.private_key.private_key_pem
-        file_permission =  "0400"
+  filename        = "/tmp/.pki/private_key.pem"
+  content         = tls_private_key.private_key.private_key_pem
+  file_permission = "0400"
 }
 resource "tls_private_key" "private_key" {
   algorithm   = "RSA"
-  rsa_bits = 2048
+  rsa_bits    = 2048
   ecdsa_curve = "P384"
 }
 resource "tls_cert_request" "csr" {
   private_key_pem = local_file.key_data.content
-  depends_on = [ local_file.key_data ]
+  depends_on      = [local_file.key_data]
 
   subject {
     common_name  = "flexit.com"
