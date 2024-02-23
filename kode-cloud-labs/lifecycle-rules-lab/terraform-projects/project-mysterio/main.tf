97d9306da7d2bfc5a4f578eef556cd8e309ccb4f
svoshall <svoshall@vivsoft.io>
2024-02-23 17:39:45 -0600

    lifecycle-rules-lab: create_before_destroy rule is dangerous

--- kode-cloud-labs/lifecycle-rules-lab/terraform-projects/project-mysterio/main.tf
+++ kode-cloud-labs/lifecycle-rules-lab/terraform-projects/project-mysterio/main.tf
@@ -2,7 +2,9 @@ resource "local_file" "file" {
     filename = var.filename
     file_permission =  var.permission
     content = random_string.string.id
-    
+    lifecycle {
+        create_before_destroy =  false # not always the best rule to use - 2 files cannot be named the same
+    }
 }
 
 resource "random_string" "string" {
